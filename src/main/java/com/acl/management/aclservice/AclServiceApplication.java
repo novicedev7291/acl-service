116dd8efcccd10843c11795084b61b6d67da2dae
kuldeep <kuldeepyadav7291@gmail.com>
2018-12-25 16:27:02 +0530

    Initial project structure

---
+++ src/main/java/com/acl/management/aclservice/AclServiceApplication.java
@@ -0,0 +1,14 @@
+package com.acl.management.aclservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/acl/management/aclservice/AclServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.acl.management.aclservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AclServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
