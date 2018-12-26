4bed6c59447d76aa8f4a46448935d2a72a7ffa3d
kuldeep <kuldeepyadav7291@gmail.com>
2018-12-26 15:39:17 +0530

    Add initial db setup script

---
+++ src/main/resources/db/migration/V2018122615271545818250__create_schema.sql
@@ -0,0 +1,175 @@
+--
+-- Table structure for table `oauth_client_details`
+--
+
+DROP TABLE IF EXISTS `oauth_client_details`;
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(255) NOT NULL,
+  `resource_ids` varchar(255) DEFAULT NULL,
+  `client_secret` varchar(255) DEFAULT NULL,
+  `scope` varchar(255) DEFAULT NULL,
+  `authorized_grant_types` varchar(255) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(255) DEFAULT NULL,
+  `authorities` varchar(255) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB;
+
+-- -----------------------------------------------------
+-- Table `companies`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE IF NOT EXISTS `companies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `code` VARCHAR(20) NOT NULL,
+  `description` VARCHAR(250) NULL,
+  `active` TINYINT NULL DEFAULT 0,
+  `customize_info` VARCHAR(2000) NOT NULL,
+  `security_info` VARCHAR(2000) NOT NULL,
+  `created_on` DATETIME NULL DEFAULT NULL,
+  `created_by` VARCHAR(100) NULL DEFAULT NULL,
+  `updated_on` DATETIME NULL DEFAULT NULL,
+  `updated_by` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `CODE_UNIQUE` (`code` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `user_name` VARCHAR(100) NULL,
+  `email` VARCHAR(200) NOT NULL,
+  `phone` VARCHAR(50) NULL,
+  `country_code` VARCHAR(5) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `change_password` TINYINT NULL DEFAULT 0,
+  `active` TINYINT NULL DEFAULT 1,
+  `company_id` VARCHAR(20) NOT NULL,
+  `created_on` DATETIME NULL DEFAULT NULL,
+  `created_by` VARCHAR(100) NULL DEFAULT NULL,
+  `updated_on` DATETIME NULL DEFAULT NULL,
+  `updated_by` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  UNIQUE INDEX `phone_UNIQUE` (`phone` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `created_on` DATETIME NULL DEFAULT NULL,
+  `created_by` VARCHAR(100) NULL DEFAULT NULL,
+  `updated_on` DATETIME NULL DEFAULT NULL,
+  `updated_by` VARCHAR(100) NULL DEFAULT ' NULL',
+  `company_id` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_roles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_roles`;
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `role_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_roles_id_idx` (`role_id` ASC),
+  INDEX `fk_users_id_idx` (`user_id` ASC),
+  CONSTRAINT `fk_roles_id`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `resource`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `resources`;
+CREATE TABLE IF NOT EXISTS `resources` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `code` VARCHAR(45) NOT NULL,
+  `created_on` DATETIME NULL DEFAULT NULL,
+  `created_by` VARCHAR(100) NULL DEFAULT NULL,
+  `updated_on` DATETIME NULL DEFAULT NULL,
+  `updated_by` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `operations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `operations`;
+CREATE TABLE IF NOT EXISTS `operations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `verb` VARCHAR(10) NOT NULL,
+  `created_on` DATETIME NULL DEFAULT NULL,
+  `created_by` VARCHAR(100) NULL DEFAULT NULL,
+  `updated_on` DATETIME NULL DEFAULT NULL,
+  `updated_by` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `permissions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `resource_id` INT NOT NULL,
+  `operation_id` INT NOT NULL,
+  `verb` VARCHAR(10) NOT NULL,
+  `link` VARCHAR(200) NOT NULL,
+  `role_id` INT NOT NULL,
+  `created_on` DATETIME NULL DEFAULT NULL,
+  `created_by` VARCHAR(100) NULL DEFAULT NULL,
+  `updated_on` DATETIME NULL DEFAULT NULL,
+  `updated_by` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_roles_id_idx` (`role_id` ASC),
+  INDEX `fk_resources_id_idx` (`resource_id` ASC),
+  INDEX `fk_operations_id_idx` (`operation_id` ASC),
+  CONSTRAINT `fk_roles_id`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_resources_id`
+    FOREIGN KEY (`resource_id`)
+    REFERENCES `resource` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_operations_id`
+    FOREIGN KEY (`operation_id`)
+    REFERENCES `operations` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
